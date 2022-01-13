203d1c0ca00ac99262e5638200bb04864d4f64be
MatthewHiggins2017 <matthew.higgins2017@gmail.com>
2022-01-13 11:02:03 +0000

    Setup File

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="primedinclusivity",
+	version="1.0.0",
+    packages=setuptools.find_packages(),
+	long_description="PrimedInclusivity Platform",
+	scripts= ['PrimedInclusivity',
+              'PIConfig'])
